f01a78ec0740ac694b72caa8874361ace8c413a5
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-13 22:56:39 +0300

    added AdminController and new resource api route

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('cars', 'CarsController@index');
 Route::get('cars/{id}', 'CarsController@show')->where('id', '[0-9]+');
+Route::resource('admin/cars', 'AdminController');
